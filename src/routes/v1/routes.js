5c575002f24df05dfacb28e8068d2b5b0b532812
tafnaz <tafnaz>
2026-01-18 13:16:13 +0530

    feat: add routes

---
+++ src/routes/v1/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authRoutes = require("../../v1/components/auth/auth.routes");
+const userRoutes = require("../../v1/components/users/user.routes");
+const taskRoutes = require("../../v1/components/tasks/task.routes");
+
+const router = express.Router();
+
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/tasks", taskRoutes);
+
+module.exports = router;
